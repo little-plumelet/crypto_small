2a7fbf6721708ce1f4328232b244bcbaf4528f44
Anastasiia Perova <perovaa.24@gmail.com>
2023-07-10 19:30:15 +0400

    add styled-components - make converter styled

--- src/components/converter/Converter.tsx
+++ src/components/converter/Converter.tsx
@@ -4,8 +4,43 @@ import { ICurrencyData } from "@/interfaces/currencyData";
 import { convertCurrencies } from "@/utils/convertCurrencies";
 import React, { useEffect, useState } from "react";
 import useSWR from "swr";
+import styled from "styled-components";
 
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+const Container = styled.div`
+  padding: 20px 50px;
+`;
+
+const ContainerCalc = styled.div`
+  padding: 20px 50px;
+  margin: 20px 0;
+  border: 1px solid gray;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+
+`;
+
+const Input = styled.input`
+  margin-bottom: 10px;
+  padding: 5px;
+`;
+
+const SelectContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 20px
+`
+const Select = styled.select`
+  padding: 5px;
+  flex: 1;
+`;
+
+const Result = styled.div`
+  margin-top: 20px;
+  font-wright: bold;
+`;
 
 export const Converter: React.FC = () => {
   const [amount, setAmount] = useState<number>(1);
@@ -15,12 +50,16 @@ export const Converter: React.FC = () => {
   const [toName, setToName] = useState<string>("");
   const [converted, setConverted] = useState(0);
   const url = `https://api.cryptorank.io/v1/currencies/?api_key=${process.env.NEXT_PUBLIC_API_KEY}&limit=200`;
-  
-  const { data: currencies, error, isLoading } = useSWR<ICurrencyData>(url, fetcher);
+
+  const {
+    data: currencies,
+    error,
+    isLoading,
+  } = useSWR<ICurrencyData>(url, fetcher);
 
   useEffect(() => {
     setConverted(convertCurrencies(amount, from, to));
-  }, [from, to, amount])
+  }, [from, to, amount]);
   if (error) {
     return <div>Error loading data</div>;
   }
@@ -36,44 +75,59 @@ export const Converter: React.FC = () => {
 
   const handlefromChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
-    const name = ((event.target.selectedOptions[0].innerText).match(/(?<=\().*?(?=\))/))?.[0];
+    const name =
+      event.target.selectedOptions[0].innerText.match(/(?<=\().*?(?=\))/)?.[0];
     setFrom(+value);
-    setFromName(name ?? '');
+    setFromName(name ?? "");
   };
 
   const handletoChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
-    const name = ((event.target.selectedOptions[0].innerText).match(/(?<=\().*?(?=\))/))?.[0];
+    const name =
+      event.target.selectedOptions[0].innerText.match(/(?<=\().*?(?=\))/)?.[0];
     setTo(+value);
-    setToName(name ?? '');
+    setToName(name ?? "");
   };
 
   if (!from && currencies.data.length > 0) {
     setFrom(currencies?.data[0]?.values?.USD?.price || 0);
-    setFromName(currencies?.data[0]?.symbol)
+    setFromName(currencies?.data[0]?.symbol);
   }
 
   if (!to && currencies.data.length > 0) {
     setTo(currencies?.data[1]?.values?.USD?.price || 0);
-    setToName(currencies?.data[1]?.symbol)
+    setToName(currencies?.data[1]?.symbol);
   }
 
   return (
-    <div>
-      <input type="number" value={amount} onChange={handleamountChange} />
-      <div>
-        <select value={from} onChange={handlefromChange}>
-          {currencies?.data.map((currency) => {
-            return <option key={currency?.id} value={currency?.values?.USD?.price}>{currency?.name} ({currency?.symbol})</option>
-          })}
-        </select>
-        <select value={to} onChange={handletoChange}>
-        {currencies?.data.map((currency) => {
-            return <option key={currency?.id} value={currency?.values?.USD?.price}>{currency?.name} ({currency?.symbol})</option>
-          })}
-        </select>
-      </div>
-      <div>{amount} {fromName} = {converted.toFixed(2)} {toName}</div>
-    </div>
+    <Container>
+      <h3>Cryptocurrency Converter Calculator</h3>
+      <ContainerCalc>
+        <Input type="number" value={amount} onChange={handleamountChange} />
+        <SelectContainer>
+          <Select value={from} onChange={handlefromChange}>
+            {currencies?.data.map((currency) => {
+              return (
+                <option key={currency?.id} value={currency?.values?.USD?.price}>
+                  {currency?.name} ({currency?.symbol})
+                </option>
+              );
+            })}
+          </Select>
+          <Select value={to} onChange={handletoChange}>
+            {currencies?.data.map((currency) => {
+              return (
+                <option key={currency?.id} value={currency?.values?.USD?.price}>
+                  {currency?.name} ({currency?.symbol})
+                </option>
+              );
+            })}
+          </Select>
+        </SelectContainer>
+        <Result>
+          {amount} {fromName} = {converted.toFixed(2)} {toName}
+        </Result>
+      </ContainerCalc>
+    </Container>
   );
 };
